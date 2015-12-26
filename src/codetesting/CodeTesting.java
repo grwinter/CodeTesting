0bc3bc1fc06259bf2654386543288da64719a815
George Winter <grwinter@Hereford.PK5001Z>
2015-12-26 06:15:31 -0500

    Initial commit

---
+++ src/codetesting/CodeTesting.java
@@ -0,0 +1,17 @@
+package codetesting;
+
+class CodeTesting {
+   public static void main(String[] args) {
+       int x = 18;
+       int y = 7;
+       boolean a, b;
+       a = false;
+       if (x/2 < y+3)
+           a = true;
+       b=true;
+       if ((x==y) || (y<x))
+       {
+           System.out.println("Got to here!");
+       }
+   }
+}
